a11901975d1fed461f503b1c79589533829bab9e
mimran0 <jmrthan@gmail.com>
2017-09-28 16:10:39 -0400

    added grouping

---
+++ FirstMavenProject/src/test/java/Group.java
@@ -0,0 +1,25 @@
+import org.testng.annotations.Test;
+
+public class Group {
+
+	@Test(groups="A")
+	public void F1(){
+		
+	}@Test(groups="B")
+	public void F2(){
+		
+	}@Test(groups="A")
+	public void F3(){
+		
+	}@Test(groups="C")
+	public void F4(){
+		
+	}@Test(groups="B")
+	public void F5(){
+		
+	}@Test(groups="A")
+	public void F6(){
+		
+	}
+	
+}
