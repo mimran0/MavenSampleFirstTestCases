5f8cd86d8a826dc5d90a324a218193e1ff09f16d
mimran0 <jmrthan@gmail.com>
2017-09-28 15:42:23 -0400

    added a calss called Prioritzation with few test annotation and gave priority. added a new xml testRunner as well

---
+++ FirstMavenProject/src/test/java/Priotization.java
@@ -0,0 +1,41 @@
+import org.testng.annotations.Test;
+
+public class Priotization {
+	@Test(priority = 0)
+	public void A() {
+		System.out.println("This is sub-procudure/method A");
+	}
+
+	@Test(priority = 4)
+	public void B() {
+		System.out.println("This is sub-procudure/method B");
+	}
+
+	@Test(priority = 5)
+	public void C() {
+		System.out.println("This is sub-procudure/method C");
+	}
+
+	@Test(priority = 2)
+	public void D() {
+		System.out.println("This is sub-procudure/method D");
+	}
+
+	@Test(priority = 3)
+	public void E() {
+		System.out.println("This is sub-procudure/method E");
+	}
+
+	@Test(priority = 1)
+	public void F() {
+		System.out.println("This is sub-procedure/method F");
+
+	}
+
+	@Test(priority = 6)
+	public void G() {
+		System.out.println("This is sub-procudure/method G");
+
+	}
+
+}
