87603e3c1ef232ed01d7670e133599804fcdcefa
mimran0 <jmrthan@gmail.com>
2017-12-25 20:31:17 -0500

    self learning

--- FirstMavenProject/src/test/java/bbb/TestSetI.java
+++ FirstMavenProject/src/test/java/bbb/TestSetI.java
@@ -1,8 +1,14 @@
 package bbb;
 
+import java.util.ArrayList;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.os.WindowsUtils;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -52,7 +58,7 @@ public class TestSetI extends afterLoginIn.CommonAPI {
 	}
 
 	// Requirement 902: Find a store
-	@Test(enabled=true)
+	@Test(enabled = true)
 	public void TC_902() {
 		String vBaseURL = "https://www.bedbathandbeyond.com/";
 		CommonAPI CommonAPI = new CommonAPI();
@@ -65,7 +71,124 @@ public class TestSetI extends afterLoginIn.CommonAPI {
 		driver.findElement(By.id("findStoreSubmitButton")).click();
 		waitTime(2000);
 		CAPTURESCREEN(driver, "BBB_TC_902_");
-		ScrollDownAndUp( driver, 500);
+		ScrollDownAndUp(driver, 500);
 
 	}
+
+	// Requirement 903: MouseOver
+	@Test(enabled = true)
+	public void TC_903_MouseOver() {
+		String vBaseURL = "https://www.bedbathandbeyond.com/";
+		CommonAPI CommonAPI = new CommonAPI();
+		WebDriver driver = CommonAPI.getDriver("FIREFOX", vBaseURL);
+		waitTime(25000);
+		// list of xpath's
+		String[] list = new String[5];
+		list[0] = "//*[@id=\"collegeBridalArea\"]/div[1]/a";
+		list[1] = "//*[@id=\"giftsNavSS\"]/a";
+		list[2] = "//*[@id=\"collegeBridalArea\"]/div[2]/a";
+		list[3] = "//*[@id=\"shopMoverLink\"]/a";
+		list[4] = "//*[@id=\"bridalGiftRegistryAnchor\"]";
+		// mouseover
+		for (String v : list) {
+			Actions action = new Actions(driver);
+			WebElement we = driver.findElement(By.xpath(v));
+			action.moveToElement(we).build().perform();
+			waitTime(2000);
+		}
+	}
+
+	// Requirement 904: System doesn't allow to login using wrong facebook credentials.
+	@Test(enabled=true)
+	public void TC_904() {
+		String vBaseURL = "https://www.bedbathandbeyond.com/";
+		CommonAPI CommonAPI = new CommonAPI();
+		WebDriver driver = CommonAPI.getDriver("FIREFOX", vBaseURL);
+		waitTime(25000);
+		driver.findElement(By.xpath("//*[@id=\"topNavMenu\"]/ul/li/a")).click();
+		waitTime(2000);
+		driver.findElement(By.xpath("//*[@id=\"newCustomer\"]/div/a/img")).click();
+		waitTime(2000);
+		// dealing with 2 tabs
+		ArrayList<String> tabs2 = new ArrayList<String>(driver.getWindowHandles());
+		driver.switchTo().window(tabs2.get(1));
+		driver.findElement(By.id("email")).sendKeys("wrongID");
+		driver.findElement(By.id("pass")).sendKeys("Wrong Password");
+		driver.findElement(By.name("login")).click();
+		CAPTURESCREEN(driver, "BBB_TC_904_");
+		// Putting ChekcPoint.
+		try {
+			boolean isErrorPresnt = driver.findElement(By.id("error_box")).isDisplayed();
+			Assert.assertTrue(isErrorPresnt);
+		} catch (Exception e) {
+			Assert.fail();
+		}
+		driver.close(); // closing the 2nd Tab
+		driver.switchTo().window(tabs2.get(0));
+	}
+	
+	
+	// Requirement 905: System ask for registration if login attempt is done with valid non registered facebook credentials.
+		@Test(enabled=true)
+		public void TC_905() {
+			String vBaseURL = "https://www.bedbathandbeyond.com/";
+			CommonAPI CommonAPI = new CommonAPI();
+			WebDriver driver = CommonAPI.getDriver("FIREFOX", vBaseURL);
+			waitTime(25000);
+			driver.findElement(By.xpath("//*[@id=\"topNavMenu\"]/ul/li/a")).click();
+			waitTime(3000);
+			driver.findElement(By.xpath("//*[@id=\"newCustomer\"]/div/a/img")).click();
+			waitTime(2000);
+			// dealing with 2 tabs
+			ArrayList<String> tabs2 = new ArrayList<String>(driver.getWindowHandles());
+			driver.switchTo().window(tabs2.get(1)); // Going to Pop-up window as second browser.
+			driver.findElement(By.id("email")).sendKeys("imranlimon00@gmail.com");
+			driver.findElement(By.id("pass")).sendKeys("1234560y");
+			System.out.println(driver.getTitle());
+			driver.findElement(By.name("login")).click();					
+			waitTime(8000);		
+			driver.switchTo().window(tabs2.get(0)); //Going back to original browser.
+			System.out.println(driver.getTitle());
+			//checkpoint (if system ask for registration instead of login then pass"
+			try {
+				boolean isRegistrationPresnt = driver.findElement(By.xpath("//*[@id=\"content\"]/div/h1")).isDisplayed();
+				Assert.assertTrue(isRegistrationPresnt);	
+			} catch (Exception e) {
+				Assert.fail();
+			}		
+			CAPTURESCREEN(driver, "BBB_TC_905_");
+			
+		}
+		
+		//Requirement 906: Users are able to fill up registration form.
+		@Test(enabled=true)
+		public void TC_906_RegistrationFormFillUp(){
+			String vBaseURL = "https://www.bedbathandbeyond.com/";
+			CommonAPI CommonAPI = new CommonAPI();
+			WebDriver driver = CommonAPI.getDriver("FIREFOX", vBaseURL);
+			waitTime(5000);
+			driver.findElement(By.xpath("//*[@id=\"topNavMenu\"]/ul/li/a")).click();
+			waitTime(3000);
+			driver.findElement(By.id("newEmail")).sendKeys("imranlimon00@gmail.com");
+			driver.findElement(By.id("newEmailBtn")).click();
+			waitTime(5000);
+			driver.findElement(By.id("firstName")).sendKeys("md");
+			driver.findElement(By.id("lastName")).sendKeys("Imran");
+			driver.findElement(By.id("basePhoneFull")).sendKeys("9119111876");
+			driver.findElement(By.id("cellPhoneFull")).sendKeys("7865432145");
+			
+			Select dropdown_ChallengeQuestions1 = new Select(driver.findElement(By.id("challengeQuestion1")));
+			dropdown_ChallengeQuestions1.selectByVisibleText("What is the name of your first crush?");
+			driver.findElement(By.id("challengeAnswer1")).sendKeys("Don't remember");
+			//waitTime(2000);
+			Select dropdown_ChallengeQuestions2 = new Select(driver.findElement(By.id("challengeQuestion2")));
+			dropdown_ChallengeQuestions2.selectByVisibleText("What is your nickname?");
+			driver.findElement(By.id("challengeAnswer2")).sendKeys("Love");
+			
+			driver.findElement(By.id("password")).sendKeys("Ilove100");
+			driver.findElement(By.id("cPassword")).sendKeys("Ilove100");
+			//canceling instead of creating
+			driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[1]/div/form/div[14]/a")).click();
+			
+		}
 }
