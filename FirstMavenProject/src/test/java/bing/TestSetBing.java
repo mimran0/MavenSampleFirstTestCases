ecdd13f5906738eb008b944ef44cb36a95ced110
mimran0 <jmrthan@gmail.com>
2018-05-06 01:06:24 -0400

    self learning

--- FirstMavenProject/src/test/java/bing/TestSetBing.java
+++ FirstMavenProject/src/test/java/bing/TestSetBing.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.os.WindowsUtils;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -37,7 +38,7 @@ public class TestSetBing extends afterLoginIn.CommonAPI {
 		String vBaseURL = "https://www.bing.com/";
 		CommonAPI CommonAPI = new CommonAPI();
 		WebDriver driver = CommonAPI.getDriver("FIREFOX", vBaseURL);
-		waitTime(25000);
+		waitTime(5000);
 
 		// ArrayList Data Structure
 		ArrayList<String> dt_MyList = new ArrayList<String>();
@@ -59,6 +60,68 @@ public class TestSetBing extends afterLoginIn.CommonAPI {
 			waitTime(2000);
 			String vSearchResult = obj_SearhResult.getText();
 			System.out.println(vSearchResult + " found for " + dt_MyList.get(i));
+		}
+	}
+
+	// Requirement 102: Users are able to view images after searching in bing
+	// web site
+	@Test(enabled = true)
+	public void TC_102_ViewImages() {
+		String vBaseURL = "https://www.bing.com/";
+		CommonAPI CommonAPI = new CommonAPI();
+		WebDriver driver = CommonAPI.getDriver("FIREFOX", vBaseURL);
+		waitTime(5000);
+
+		// ArrayList Data Structure
+		ArrayList<String> dt_MyList = new ArrayList<String>();
+		dt_MyList.add(0, "Red Rose");
+		dt_MyList.add(1, "bird");
+		dt_MyList.add(2, "rain");
+		dt_MyList.add(3, "water");
+		dt_MyList.add(4, "md shahajada imran");
+
+		// for loop
+		for (int i = 0; i < dt_MyList.size(); i++) {
+			driver.get("https://www.bing.com/");
+			driver.findElement(By.tagName("input")).sendKeys(dt_MyList.get(i));
+			waitTime(3000);
+			driver.findElement(By.className("b_searchboxSubmit")).click();
+			waitTime(5000);
+			driver.findElement(By.linkText("Images")).click();
+			waitTime(5000);
+			scrolldown(driver, 500);
+			if ((dt_MyList.get(i)).equals("md shahajada imran")) {
+				driver.findElement(By.linkText("All")).click();
+				waitTime(3000);
+				CAPTURESCREEN(driver, "Bing_md_shahajada_imran");
+			} else {// don't capture screenshot))
+			}
+		}
+	}
+
+	// Requirement 103: Expected message display on specified
+	// (home>rewords>Microsoft) page.
+	@Test(enabled = true)
+	public void TC_103_ExpectedMessageDisplay() {
+		String vBaseURL = "https://www.bing.com/";
+		CommonAPI CommonAPI = new CommonAPI();
+		WebDriver driver = CommonAPI.getDriver("CHROME", vBaseURL);
+		waitTime(5000);
+		driver.findElement(By.id("id_rh")).click();
+		waitTime(5000);
+		// driver.findElement(By.cssSelector(".msflyout-join-btn")).click();
+		// faking as above line does not work.
+		driver.get("https://www.bing.com/explore/rewards?FORM=MA1377&wt.mc_id=MA1377&PUBL=BINGCOM&CREA=MA1377");
+		waitTime(3000);
+		WebElement obj_Checkpoint = driver.findElement(By.cssSelector("#signIn > div:nth-child(2) > h1:nth-child(3)"));
+		HighLight_Element(driver, obj_Checkpoint);
+
+		if ((obj_Checkpoint.getText()).equals("Earn free rewards from Microsoft")) {
+			System.out.println("Test passed");
+		} else {
+			Assert.fail("Did not display expected message");
+			System.out.println("It supposed to display 'Earn free rewards from Microsoft' but displayed "
+					+ obj_Checkpoint.getText());
 		}
 	}
 
