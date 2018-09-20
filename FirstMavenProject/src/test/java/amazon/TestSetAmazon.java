102151dff4de948c17b8008480e9d0b640939293
mimran0 <jmrthan@gmail.com>
2018-09-20 09:23:47 -0400

    self learning

--- FirstMavenProject/src/test/java/amazon/TestSetAmazon.java
+++ FirstMavenProject/src/test/java/amazon/TestSetAmazon.java
@@ -15,6 +15,7 @@ import org.testng.annotations.Test;
 
 import afterLoginIn.CommonAPI;
 import junit.framework.Assert;
+
 /**
  * @author md shahajada imran
  *
@@ -109,7 +110,7 @@ public class TestSetAmazon extends afterLoginIn.CommonAPI {
 			r.keyRelease(KeyEvent.VK_ENTER);
 			// Hit enter from the keyboard Ends here
 			waitTime(5000);
-			//CAPTURESCREEN (driver);
+			// CAPTURESCREEN (driver);
 			driver.findElement(By.id("twotabsearchtextbox")).clear();
 			i++;
 		}
